ae9b9127ca3f30f1154b37987e6909b2e4fceba4
Kelly Lougheed <kellylougheed@gmail.com>
2017-04-21 14:17:28 -0700

    Unicorn app

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
 
+    @IBAction func helloWorld(_ sender: Any) {
+        textDisplay.text = "Hello, world!"
+        print("Pressed Hello World button")
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        textDisplay.text = "Goodbye, world!"
+        print("Pressed Goodbye World button")
+    }
+    
+    @IBAction func unicorn(_ sender: Any) {
+        textDisplay.text = "🦄"
+        print("Pressed Unicorn! button")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
